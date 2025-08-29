7046adc9064f9e0f513da2c85b357a89362fb581
Nicolas Vieira Maria <nicolasvmaria1@gmail.com>
2025-08-29 15:49:38 -0300

    nova landing page

--- script/script.js
+++ script/script.js
@@ -1,9 +1,16 @@
 window.addEventListener("scroll", function () {
-    const scrollY = window.scrollY;
-    
-    if (scrollY > 700) { // valor em pixels para quando a troca ocorre
-      document.body.style.backgroundColor = "#000000"; // nova cor
-    } else {
-      document.body.style.backgroundColor = "#ffcf00"; // cor original
-    }
-  });
+  const scrollY = window.scrollY;
+
+  // Se o scroll estiver na PRIMEIRA página
+  if (scrollY < 650) {
+      document.body.style.backgroundColor = "#ffcf00"; 
+  }
+  // Se o scroll estiver na SEGUNDA página
+  else if (scrollY >= 600 && scrollY < 1800) {
+      document.body.style.backgroundColor = "#000000"; 
+  }
+  // Se o scroll estiver na TERCEIRA página
+  else {
+      document.body.style.backgroundColor = "#ffcf00"; 
+  }
+});
